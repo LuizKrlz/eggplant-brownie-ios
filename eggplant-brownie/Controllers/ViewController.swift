c0588eb7db55fa563e037f9ae901ea9bdc61e421
Luiz Correa <luiz.correa@laborit.com.br>
2021-02-09 08:40:56 -0300

    Implement a custom table view, capture select item event and apply a mark type in each row

--- eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/Controllers/ViewController.swift
@@ -12,13 +12,59 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // MARK: - Atributos
     var delegate: AdicionaRefeicaoDelegate?
+    var itens: [Item] = [
+        Item(nome: "Molho de tomate", calorias: 40.0),
+        Item(nome: "Queijo", calorias: 60.0),
+        Item(nome: "Molho de Pimenta", calorias: 10.0),
+        Item(nome: "Massa", calorias: 40.0),
+        Item(nome: "Orégano", calorias: 2.0),
+        Item(nome: "Cebola", calorias: 5.0),
+    ]
+    
+    var itensSelecionados: [Item] = []
+    
+    // MARK: - IBOutlets
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var felicidadeTextField: UITextField?
     
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        celula.textLabel?.text = itens[indexPath.row].nome
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    // captura a seleção do usuario na tabela
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        let item = itens[indexPath.row]
+        
+        if (celula.accessoryType == .none) {
+            celula.accessoryType = .checkmark
+            itensSelecionados.append(item)
+        } else {
+            celula.accessoryType = .none
+            
+            if let position = itensSelecionados.firstIndex(of: item) {
+                itensSelecionados.remove(at: position)
+            }
+        }
+        
+    }
+    
+    // MARK: - IBActions
     @IBAction func adicionar() {
                 
         guard let nomeDaRefeicao = nomeTextField?.text else {
@@ -29,8 +75,8 @@ class ViewController: UIViewController {
             return
         }
         
-        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
-        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
+        refeicao.itens = itensSelecionados
         
         // remove screen stack
         delegate?.add(refeicao)
